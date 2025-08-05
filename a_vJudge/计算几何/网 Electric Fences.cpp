64d1a8ba6c62889a2850afed2c7d4ab506818e39
niuerben <niuerben@outlook.com>
2025-08-05 12:08:21 +0800

    2025.8.5

--- a_vJudge/计算几何/网 Electric Fences.cpp
+++ a_vJudge/计算几何/网 Electric Fences.cpp
@@ -1,27 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N = 2e5+10, inf = 1e9, mod = 1e9+7;
-int l[N],r[N],ans;
+const int N = 2e6+10, inf = 1e9, mod = 1e9+7;
+const double eps = 1e-6;
 double n,m,p;
+int l[N],r[N],ans;
 
 signed main() {
-    cin>>n>>m>>p;
-    double k1 = m/n;
-    for(int i=1;i<m;i++){
-        l[i] = ceil(k1*i);
-    } 
+	cin>>n>>m>>p;
+	for(int i=1;i<m;i++){
+		l[i] = ceil(n*i/m+eps);
+	}
 
-    for(int i=1;i<m;i++){
-        
-        r[i] = floor(i*n/(m-p)+p);
-    }
+	for(int i=1;i<m;i++){
+		r[i] = floor((n-p)*i/m+p-eps);
+		// cout<<(n-p)*i/m+p<<'\n';
+	}
 
-    for(int i=1;i<m;i++){
-        cout<<l[i]<<" "<<r[i]<<endl;
-        ans+=r[i]-l[i]+1;
-    }
-    cout<<ans;
-    return 0;
+	for(int i=1;i<m;i++){
+		// cout<<l[i]<<" "<<r[i]<<endl;
+		ans += r[i]-l[i]+1;
+	}
+
+	cout<<ans;
 }
-    
+	
